e41339eafd47bd39d49ac54f34901dcd3e0bce01
Vuong Quang Tu <c0708h@gmail.com>
2026-04-23 13:36:02 +0700

    fix spouse render

--- src/components/Tree.jsx
+++ src/components/Tree.jsx
@@ -1,20 +1,34 @@
 import Node from './Node'
 
 export default function Tree({ node }) {
+  if (!node) return null
+
   return (
     <div className="flex flex-col items-center">
-      <div className="flex gap-4">
+      
+      {/* Couple */}
+      <div className="flex items-center gap-2">
         <Node data={node.person} />
-        {node.spouse && <Node data={node.spouse} />}
+
+        {node.spouse && (
+          <>
+            {/* line nối vợ chồng */}
+            <div className="w-6 h-px bg-gray-400"></div>
+
+            <Node data={node.spouse} />
+          </>
+        )}
       </div>
 
+      {/* Line xuống con */}
       {node.children.length > 0 && (
         <div className="w-px h-6 bg-gray-400"></div>
       )}
 
-      <div className="flex gap-8">
-        {node.children.map((child, i) => (
-          <Tree key={i} node={child} />
+      {/* Children */}
+      <div className="flex gap-8 mt-2">
+        {node.children.map((child) => (
+          <Tree key={child.person.id} node={child} />
         ))}
       </div>
     </div>
