ce670cf1dac7fc292a4541165a993ff2a5046cc5
Vuong Quang Tu <c0708h@gmail.com>
2026-04-23 11:40:47 +0700

    update family data

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "/giaphavuongpro/",
+  base: "/giaphavuongpro/",   // 👈 QUAN TRỌNG
   plugins: [react()]
 })
